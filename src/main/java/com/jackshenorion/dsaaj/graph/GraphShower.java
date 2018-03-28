57e1b97a1cf9e199b79e343a2951c6506b08a670
Jack Shen <jack.shen@nasdaq.com>
2018-03-28 11:15:09 +1100

    add sample code for vertex and edge style

--- src/main/java/com/jackshenorion/dsaaj/graph/GraphShower.java
+++ src/main/java/com/jackshenorion/dsaaj/graph/GraphShower.java
@@ -4,8 +4,11 @@ import com.jackshenorion.dsaaj.circle.CircleCoordinateProvider;
 import com.jackshenorion.dsaaj.circle.Coordinate;
 import com.jackshenorion.dsaaj.graph.intf.IEdge;
 import com.jackshenorion.dsaaj.graph.intf.IGraph;
+import com.mxgraph.model.mxCell;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
 import com.mxgraph.view.mxGraph;
+import com.mxgraph.view.mxStylesheet;
 
 import javax.swing.*;
 import java.awt.*;
@@ -24,6 +27,7 @@ public class GraphShower<V> extends JFrame {
     private IGraph<V> graphData;
     private int maxX = 400;
     private int maxY = 320;
+    private mxGraph graph = new mxGraph();
 
     public GraphShower(IGraph<V> graphData) throws HeadlessException {
         super("Graph Shower");
@@ -32,9 +36,9 @@ public class GraphShower<V> extends JFrame {
     }
 
     private void init() {
-        mxGraph graph = new mxGraph();
         Object parent = graph.getDefaultParent();
         graph.getModel().beginUpdate();
+        createStyle();
         try {
             Map<V, Object> vertexToCell = new HashMap<>();
             Collection<V> vertices = graphData.getAllVertexes();
@@ -46,17 +50,81 @@ public class GraphShower<V> extends JFrame {
                 maxX = Math.max(thisX, maxX);
                 maxY = Math.max(thisY, maxY);
                 Object cell = graph.insertVertex(parent, null, v, thisX, thisY, itemWidth, itemHeight);
+                if (seq % 3 == 0) {
+                    ((mxCell) cell).setStyle("whiteVertex");
+                } else if (seq % 3 == 1) {
+                    ((mxCell) cell).setStyle("greyVertex");
+                } else {
+                    ((mxCell) cell).setStyle("blackVertex");
+                }
                 vertexToCell.put(v, cell);
                 seq++;
             }
+
+            int edgeSeq = 0;
             for (IEdge<V> edge : graphData.getAllEdges()) {
-                graph.insertEdge(parent, null, edge.getWeight(), vertexToCell.get(edge.getSource()), vertexToCell.get(edge.getTarget()));
+                if (edgeSeq % 3 == 0) {
+                    graph.insertEdge(parent, null, edge.getWeight(), vertexToCell.get(edge.getSource()), vertexToCell.get(edge.getTarget()), "defEdge;directedEdge");
+                } else if (edgeSeq % 3 == 1) {
+                    graph.insertEdge(parent, null, edge.getWeight(), vertexToCell.get(edge.getSource()), vertexToCell.get(edge.getTarget()), "dashEdge;undirectedEdge");
+                } else {
+                    graph.insertEdge(parent, null, edge.getWeight(), vertexToCell.get(edge.getSource()), vertexToCell.get(edge.getTarget()), "boldEdge;directedEdge");
+                }
+                edgeSeq++;
             }
         } finally {
             graph.getModel().endUpdate();
         }
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
         getContentPane().add(graphComponent);
+    }
+
+    private void createStyle() {
+        Map<String, Object> whiteVertex = new HashMap<>();
+        whiteVertex.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+        whiteVertex.put(mxConstants.STYLE_FILLCOLOR, "#FFFFFF");
+        whiteVertex.put(mxConstants.STYLE_FONTCOLOR, "#000000");
+
+        Map<String, Object> greyVertex = new HashMap<>();
+        greyVertex.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+        greyVertex.put(mxConstants.STYLE_FILLCOLOR, "grey");
+        greyVertex.put(mxConstants.STYLE_FONTCOLOR, "black");
+
+        Map<String, Object> blackVertex = new HashMap<>();
+        blackVertex.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+        blackVertex.put(mxConstants.STYLE_FILLCOLOR, "black");
+        blackVertex.put(mxConstants.STYLE_FONTCOLOR, "white");
+
+        Map<String, Object> defEdge = new HashMap<>();
+        defEdge.put(mxConstants.STYLE_STROKEWIDTH, 1);
+        defEdge.put(mxConstants.STYLE_STROKECOLOR, "black");
+        defEdge.put(mxConstants.STYLE_DASHED, false);
+
+        Map<String, Object> boldEdge = new HashMap<>();
+        boldEdge.put(mxConstants.STYLE_STROKEWIDTH, 3);
+        boldEdge.put(mxConstants.STYLE_STROKECOLOR, "black");
+        boldEdge.put(mxConstants.STYLE_DASHED, false);
+
+        Map<String, Object> dashEdge = new HashMap<>();
+        dashEdge.put(mxConstants.STYLE_STROKEWIDTH, 1);
+        dashEdge.put(mxConstants.STYLE_STROKECOLOR, "black");
+        dashEdge.put(mxConstants.STYLE_DASHED, true);
+
+        Map<String, Object> undirectedEdge = new HashMap<>();
+        undirectedEdge.put(mxConstants.STYLE_ENDARROW, "none");
+
+        Map<String, Object> directedEdge = new HashMap<>();
+        directedEdge.put(mxConstants.STYLE_ENDARROW, mxConstants.ARROW_CLASSIC);
+
+        mxStylesheet stylesheet = graph.getStylesheet();
+        stylesheet.putCellStyle("whiteVertex", whiteVertex);
+        stylesheet.putCellStyle("greyVertex", greyVertex);
+        stylesheet.putCellStyle("blackVertex", blackVertex);
+        stylesheet.putCellStyle("defEdge", defEdge);
+        stylesheet.putCellStyle("boldEdge", boldEdge);
+        stylesheet.putCellStyle("dashEdge", dashEdge);
+        stylesheet.putCellStyle("undirectedEdge", undirectedEdge);
+        stylesheet.putCellStyle("directedEdge", directedEdge);
     }
 
     public void doShow() {
